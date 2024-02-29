156a7146732368f560045a86242c9477c1b524c0
Gavin Jackson <gavinjackson4101@yahoo.com>
2024-02-28 22:59:22 -0500

    change2

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme. Please click "Log in" to continue into the dashboard.</strong>
+            <strong>Welcome to Acme. Please click Log in to continue</strong>
           </p>
           <Link
             href="/dashboard"
